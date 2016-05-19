42f319b60a5c4cdc283b08c616e6b08def8a8f82
Joshua Leihe <jleihe@gmail.com>
2016-05-19 14:30:19 -0700

    main.py can now retrieve tide information

--- Console/functions.py
+++ Console/functions.py
@@ -2,5 +2,25 @@
 # Written By: Joshua Leihe
 # Filename: function.py
 
-def test():
-	print "Success!" 
+import untangle
+import datetime
+
+obj = untangle.parse('Data/Tides/GoldenGateBridge.xml')
+data = obj.datainfo.data
+item = data.item
+
+
+#Returns the tide on a given day and time
+def tide(date, time):
+	for i in item:
+		date_parts = {}
+		d = i.date.cdata
+		d = d.split('/')
+		
+    		item_date = datetime.date(int(d[0]), int(d[1]), int(d[2]))
+    		time = i.time.cdata
+    		h_l = i.highlow.cdata
+		pred = i.predictions_in_ft.cdata
+    		
+		if item_date == date:
+			print time + " -- " +pred + " " + h_l
--- Console/main.py
+++ Console/main.py
@@ -2,7 +2,22 @@
 # Writen By: Joshua Leihe
 # Filename: main.py
 
-from functions import test
-print "The main program!"
+import datetime
+from functions import *
 
-test()
+t = datetime.time (1, 1, 1)
+d = datetime.date(2016, 5, 19)
+
+print """Please Enter the Year, Monty, and Day to retrieve\ntide data for the Golden Gate Bridge.\n\n"""
+
+r = 'y'
+while r != 'n':
+	y = input("Year: ")
+	m = input("Month: ")
+	d = input ("Day: ")
+	
+	date = datetime.date(y, m, d)
+	
+	print "\nThe tides on " + str(date) + " are:\n"
+	tide(date, t)
+	r = input("Continue? (y/n)")
